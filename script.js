12feb8ebdd49b77f543c936c13241f9db18b63cd
gkrockz <61354468+gkrockz@users.noreply.github.com>
2021-09-16 22:33:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// calls driver function 
+function fetchInfo() {
+    var code = document.getElementById("usrInp").value;
+    fetchGif(code);
+}
+
+// fetch GIF/jpg based on HTTP status code
+function fetchGif(code) {
+    try {
+        var status = document.getElementById("code");
+        var codeInfo = document.getElementById("codeInfo");
+        var gif =  document.getElementById("gif");
+        var info = isCodeValid(code);
+        document.getElementById("usrInp").value = "";
+        status.textContent = `Response code: ${code}`;
+
+        if(info != false) { 
+            gif.style.display="flex";
+            gif.src = `https://vadivelu.anoram.com/gif/${code}`;
+            gif.onerror = function () {
+                console.log('image not available in gif format, so Loading it in jpg format');
+                this.src = `https://vadivelu.anoram.com/jpg/${code}`;       
+            };
+            codeInfo.textContent = `Status: ${info}`;
+        }
+        else {
+            gif.style.display="none";
+            codeInfo.textContent=`Sorry, no image/gif avaiable for this status code`;
+        }
+    }   
+    catch(error) {
+        console.log(error);
+    }
+}
+
+// retrieve response info 
+function isCodeValid(code) {
+  var status = false;
+  for (var i = 0 ; i<statusCode.length;i++) {
+        if(statusCode[i].statusCode == code) {
+            status = true;
+            return statusCode[i].info;
+        }
+    }
+    if(!status) {
+        var codeInfo = document.getElementById("codeInfo");
+        codeInfo.textContent = "";
+        return status;
+    }
+}
---
+++ statusCode.js
@@ -0,0 +1,125 @@
+// status codes with their response info 
+
+const statusCode = [
+    {
+        statusCode:100,
+        info:"Continue"
+    },
+    {
+        statusCode:101,
+        info:"Switching Protocols"
+    },
+    {
+        statusCode:200,
+        info:"OK"
+    },
+    {
+        statusCode:201,
+        info:"Created"
+    },
+    {
+        statusCode:206,
+        info:"Partial Content"
+    },
+    {
+        statusCode:301,
+        info:"Moved Permanently"
+    },
+    {
+        statusCode:302,
+        info:"Found"
+    },
+    {
+        statusCode:307,
+        info:"Temporary Redirect"
+    },
+    {
+        statusCode:400,
+        info: "Bad Request"
+    },
+    {
+        statusCode:401,
+        info: "Unauthorized"
+    },
+    {
+        statusCode:402,
+        info: "Payment Required"
+    },
+    {
+        statusCode:403,
+        info: "Forbidden"
+    },
+    {
+        statusCode:404,
+        info: "Not Found"
+    },
+    {
+        statusCode:405,
+        info: "Not Allowed"
+    },
+    {
+        statusCode:406,
+        info: "Not Accectable"
+    },
+    {
+        statusCode:408,
+        info: "Request Timeout"
+    },
+    {
+        statusCode:409,
+        info:"Conflict"  
+    },
+    {
+        statusCode:410,
+        info:"Gone"  
+    },
+    {
+        statusCode:411,
+        info: "Length Required"  
+    },
+    {
+        statusCode:412,
+        info: "Precondition Failed"  
+    },
+    {
+        statusCode:417,
+        info:"Expectation Failed"  
+    },
+    {
+        statusCode:421,
+        info:"Misdirect Request"
+    },
+    {
+        statusCode:422,
+        info:"Unprocessable Entity"  
+    },
+    {
+        statusCode:423,
+        info: "Locked"
+    },
+    {
+        statusCode: 424,
+        info: "Failed Dependency"
+    },
+    {
+        statusCode:499,
+        info: "Client Closed Request"
+    },
+    {
+        statusCode:500,
+        info: "Internal Server Error"
+    },
+    {
+        statusCode:502,
+        info: "Bad Gateway"
+    },
+    
+    {
+        statusCode:506,
+        info: "Variant Also Negotiates"
+    },
+    {
+        statusCode:508,
+        info:"Loop Detected"
+    }
+];
